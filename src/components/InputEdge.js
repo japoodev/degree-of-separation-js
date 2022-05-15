83ba2291ae96a9906aa7e99c8dd848d6bafd1616
japoodev <jafarsaadique@gmail.com>
2022-05-16 02:46:10 +0400

    Made the friend option as selector instead of input text

--- src/components/InputEdge.js
+++ src/components/InputEdge.js
@@ -5,7 +5,7 @@ function InputEdge(props) {
         {
             name1: "",
             name2: "",
-            relationship: ""
+            relationship: "friend"
         }
     );
 
@@ -22,18 +22,21 @@ function InputEdge(props) {
         setEdge({
             name1: "",
             name2: "",
-            relationship: ""
+            relationship: "friend"
         });
     }
         
   return (
     <form className="mb-4">
         <h3 className="ml-2">Add relationship between two people</h3>
-            <input className="border-2 rounded-md h-8 m-2" type="text" placeholder='Enter first name' value={edge.name1} onChange={handleChange} name="name1" />
+            <input className="border-2 rounded-md h-8 m-2" type="text" placeholder='First person' value={edge.name1} onChange={handleChange} name="name1" />
 
-            <input className="border-2 rounded-md h-8 m-2" type="text" placeholder='Enter second name' value={edge.name2} onChange={handleChange} name="name2" />
-        
-            <input className="border-2 rounded-md h-8 m-2" type="text" placeholder='Enter relationship' value={edge.relationship} onChange={handleChange} name="relationship" />
+            <input className="border-2 rounded-md h-8 m-2" type="text" placeholder='Second person' value={edge.name2} onChange={handleChange} name="name2" />
+
+            <select className="border-2 rounded-md h-8 m-2" value={edge.relationship} onChange={handleChange} name="relationship">
+                <option value="friend">Friend</option>
+            </select>
+            {/* <input className="border-2 rounded-md h-8 m-2" type="text" placeholder='Enter relationship' value={edge.relationship} onChange={handleChange} name="relationship" /> */}
         
         <input className="rounded-md bg-indigo-800 text-white text-sm h-8 w-14 m-2" type="submit" value="Add" onClick={handleSubmit} />
     </form>
